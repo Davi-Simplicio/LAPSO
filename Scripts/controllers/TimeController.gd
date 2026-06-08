5bd34736fb5c4ee5119f24e51bf32b90f29b1dfe
gabriel-Koehler <ggaabbrriieellks@gmail.com>
2026-06-08 17:37:21 -0300

    adicionando filtro e alterando codigo no time controller, além de colocar colisão nos moveis

--- Scripts/controllers/TimeController.gd
+++ Scripts/controllers/TimeController.gd
@@ -8,15 +8,26 @@ var Future = null;
 
 func _ready():
 	# Conecta o sinal da miniatura à função local do mapa
+	
 	Past = get_node("Past");
 	Present = get_node("Present");
 	Future = get_node("Future");
+	
+	
+		
 	miniatura_elevador.map_update_requested.connect(_on_map_update_requested)
 	
 func _on_map_update_requested(action_data: int):
 	Past.visible = action_data == 1
 	Present.visible = action_data == 2
 	Future.visible = action_data == 3
+	
+	Past.get_node("hotel_decoration").collision_enabled = Past.visible == true
+	Present.get_node("hotel_decoration").collision_enabled = Present.visible == true
+	Future.get_node("hotel_decoration").collision_enabled = Future.visible == true
+	Past.get_node("hotel_decoration_details").collision_enabled = Past.visible == true
+	Present.get_node("hotel_decoration_details").collision_enabled = Present.visible == true
+	Future.get_node("hotel_decoration_details").collision_enabled = Future.visible == true
 	
 	Past.process_mode = Node.PROCESS_MODE_INHERIT if action_data == 1 else Node.PROCESS_MODE_DISABLED
 	Present.process_mode = Node.PROCESS_MODE_INHERIT if action_data == 2 else Node.PROCESS_MODE_DISABLED
