8d6ab43716d6423c7c6aa48b19ce927470599146
DAVI SIMPLÍCIO <davi.simplicio@catolicasc.edu.br>
2026-06-09 21:22:40 -0300

    top

--- Scripts/DialogueTopic.gd
+++ Scripts/DialogueTopic.gd
@@ -2,9 +2,9 @@
 class_name DialogueTopic
 extends Resource
 
-@export var button_label: String = "Ask about..."
+@export var button_label: String = "Diga Alguma coisa"
 # If this is empty, the button always shows. 
 # If you type "found_post_it", the button only shows if GameState has that fact.
 @export var required_fact_id: String = "" 
 
-@export_multiline var lines: Array[String] = ["Player: ...", "NPC: ..."]
+@export_multiline var lines: Array[String] = ["José: ...", "NPC: ..."]
